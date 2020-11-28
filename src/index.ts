cc8acb2a5fd349a8e6f9ef899a5eda2ea2515ba7
Anand Simmy <anandsimmy7@gmail.com>
2020-11-28 20:18:23 +0530

    added jupyter lab commands

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets'
 
 /**
  * Initialization data for the apod-lab-extension extension.
@@ -9,8 +8,27 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'apod-lab-extension',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    const content= new Widget()
+    const widget= new MainAreaWidget({ content })
+    widget.id= 'apod-jupyterlab'
+    widget.title.label= 'Astronomy Picture'
+    widget.title.closable= true
+    const command:string= 'apod:open'
+    app.commands.addCommand(command, {
+      label: 'Astrnomy Picture',
+      execute:() => {
+        if(!widget.isAttached){
+          app.shell.add(widget, 'main')
+        }else{
+          app.shell.activateById(widget.id)
+        }
+      }
+    });
+    palette.addItem({command, category: 'Tutorial'})
     console.log('JupyterLab extension apod-lab-extension is activated!');
+    console.log('Palette', palette)
   }
 };
 
